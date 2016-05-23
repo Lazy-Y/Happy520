c96f2d6c91925416580ac6a9283e4a3f77acbf2e
Sherry Zhou <sherry_zhou_427@hotmail.com>
2016-05-22 23:32:52 -0700

    Initial Commit

---
+++ Happy520/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Happy520
+//
+//  Created by ZhouShuxiao on 5/22/16.
+//  Copyright © 2016 ZhouShuxiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
